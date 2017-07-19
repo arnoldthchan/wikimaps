c0426189aefa408b268037424b27cca0e38ed485
TakNg <tak_ng@yahoo.com>
2017-07-19 18:17:32 +0000

    migration

---
+++ db/migrations/20170719153310_migration_name.js
@@ -0,0 +1,41 @@
+exports.up = function(knex, Promise) {
+  return knex.schema
+    .createTable('users', function (table) {
+      table.increments('id');
+      table.string('name');
+      table.string('email');
+      table.string('password');
+    })
+    .createTable('maps', function (table) {
+      table.increments('id');
+      table.integer('creator_id').references('users.id');
+      table.string('title');
+      table.decimal('latitude');
+      table.decimal('longitude');
+    })
+    .createTable('points', function (table) {
+      table.increments('id');
+      table.string('title');
+      table.string('description');
+      table.string('image');
+      table.decimal('latitude');
+      table.decimal('longitude');
+      table.integer('map_id');//.references('maps.id');
+      table.integer('user_id');//.references('users.id');
+    })
+    .createTable('users_maps', function(table){
+      table.integer('user_id').references('users.id');
+      table.integer('map_id').references('maps.id');
+      table.boolean('favourite');
+      table.boolean('contribution');
+    })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema
+    .dropTable('users_maps')
+    .dropTable('points')
+    .dropTable('maps')
+    .dropTable('users')
+};
+
