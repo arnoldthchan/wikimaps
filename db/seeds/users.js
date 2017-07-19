117f93b341d57091bcd33c5915b2bafa802c2283
Arnold Chan <arnoldthchan@gmail.com>
2017-07-19 19:48:51 +0000

    Added 2 more users

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -5,6 +5,8 @@ exports.seed = function(knex, Promise) {
         knex('users').insert({name: 'Alice', email: 'Alice@email.com', password: 'Alice'}),
         knex('users').insert({name: 'Bob', email: 'Bob@email.com', password: 'Bob'}),
         knex('users').insert({name: 'Charlie', email: 'Charlie@email.com', password: 'Charlie'})
+        knex('users').insert({name: 'Devin', email: 'Devin@email.com', password: 'Devin'})
+        knex('users').insert({name: 'Earl', email: 'Earl@email.com', password: 'Earl'})
       ]);
     });
 };
