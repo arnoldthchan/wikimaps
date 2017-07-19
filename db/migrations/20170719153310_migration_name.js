fb9847f1b15a46f9fec4449567d11cb5e1f84d50
TakNg <tak_ng@yahoo.com>
2017-07-19 19:00:42 +0000

    migration

--- db/migrations/20170719153310_migration_name.js
+++ db/migrations/20170719153310_migration_name.js
@@ -1,5 +1,6 @@
 exports.up = function(knex, Promise) {
   return knex.schema
+    .dropTable('users')
     .createTable('users', function (table) {
       table.increments('id');
       table.string('name');
