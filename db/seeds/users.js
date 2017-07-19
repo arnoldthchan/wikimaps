7bae606c9fa30eff65b0bedc7e322cb8dccdc148
Arnold Chan <arnoldthchan@gmail.com>
2017-07-19 19:43:44 +0000

    Updated users seed file for accurate columns

--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({name: 'Alice', email: 'Alice@email.com', password: 'Alice'}),
+        knex('users').insert({name: 'Bob', email: 'Bob@email.com', password: 'Bob'}),
+        knex('users').insert({name: 'Charlie', email: 'Charlie@email.com', password: 'Charlie'})
       ]);
     });
 };
