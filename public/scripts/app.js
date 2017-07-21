e380022c6a9ec8e2658aba1009976068bfd7a028
Arnold Chan <arnoldthchan@gmail.com>
2017-07-21 21:15:13 +0000

    Added more SCSS and bootstrap

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
 
 $(document).ready(() => {
+  var map;
   $.ajax({
     method: "GET",
     url: "/maps"
@@ -9,7 +10,6 @@ $(document).ready(() => {
       let item = $('<li>').append(link);
       $('ul.list').append(item);
     }
-    return
   });
 });
 
--- public/scripts/login-box.js
+++ public/scripts/login-box.js
@@ -19,15 +19,19 @@ $(document).ready(function() {
     // Add the mask to body
     $('body').append('<div id="mask"></div>');
     $('#mask').fadeIn(300);
-
-    return false;
   });
+
+
 
   // When clicking on the button close or the mask layer the popup closed
   $('a.close, #mask').on('click', function() {
     $('#mask , .login-popup').fadeOut(300 , function() {
       $('#mask').remove();
     });
-    return false;
+  });
+
+  $("#favs").click(function(event) {
+    event.preventDefault();
+    $("#wrapper").toggleClass("toggled");
   });
 });
