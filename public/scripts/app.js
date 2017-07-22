94f197b8c5a0fc36624296832533cc56d1cc3c42
Arnold Chan <arnoldthchan@gmail.com>
2017-07-22 18:17:12 +0000

    list works based on mapid, reduced CSS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,30 +8,38 @@ $(document).ready(() => {
     url: "/maps"
   }).done((maps) => {
     for(map of maps) {
-      let link = $('<a>').attr('href',`/maps/${map.id}`).text(map.title)
-      let item = $('<li>').append(link);
+      let link = $(`<a>`)
+      // let link = $('<a>').attr('href',`/maps/${map.id}`).text(map.title)
+      let item = $(`<li data-mapid="${map.id}">`).data('asd', map.id).attr("class", "listItem").text(map.title);
       $('ul.list').append(item);
     }
   });
 
-  $(document).on('keyup', (event) => {
-    switch (event.which) {
-      case 49:
-        displayNewMap(1);
-        break;
-      case 50:
-        displayNewMap(2);
-        break;
-      case 51:
-        displayNewMap(3);
-        break;
-      case 52:
-        displayNewMap(4);
-        break;
-      case 53:
-        displayNewMap(5);
-        break;
-    }
+  $("ul.nav").on('click', '.listItem', (event) => {
+    let mapid= $(event.target).data('mapid');
+    displayNewMap(mapid);
   });
+
+  //TEST WITH NUM KEYS (1-5)
+  // $(document).on('keyup', (event) => {
+  //   switch (event.which) {
+  //     case 49:
+  //       displayNewMap(1);
+  //       break;
+  //     case 50:
+  //       displayNewMap(2);
+  //       break;
+  //     case 51:
+  //       displayNewMap(3);
+  //       break;
+  //     case 52:
+  //       displayNewMap(4);
+  //       break;
+  //     case 53:
+  //       displayNewMap(5);
+  //       break;
+  //   }
+  // });
+
 });
 
