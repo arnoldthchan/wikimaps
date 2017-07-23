e9985426db3e4ef70fb02e52946cb8f879370c21
Arnold Chan <arnoldthchan@gmail.com>
2017-07-23 15:40:20 +0000

    added profile page to main page as a SPA

--- public/scripts/login-box.js
+++ public/scripts/login-box.js
@@ -1,30 +1,16 @@
 $(document).ready(function() {
   $('a.login-window').click(function() {
 
-    //Getting the variable's value from a link
-    var loginBox = $(this).attr('href');
-
     //Fade in the Popup
-    $(loginBox).fadeIn(300);
-
-    //Set the center alignment padding + border see css style
-    var popMargTop = ($(loginBox).height() + 24) / 2;
-    var popMargLeft = ($(loginBox).width() + 24) / 2;
-
-    $(loginBox).css({
-        'margin-top' : -popMargTop,
-        'margin-left' : -popMargLeft
-    });
+    $('#login-box').fadeIn(300);
 
     // Add the mask to body
     $('body').append('<div id="mask"></div>');
     $('#mask').fadeIn(300);
   });
 
-
-
   // When clicking on the button close or the mask layer the popup closed
-  $('a.close, #mask').on('click', function() {
+  $('body').on('click', 'a.close, #mask', function() {
     $('#mask , .login-popup').fadeOut(300 , function() {
       $('#mask').remove();
     });
